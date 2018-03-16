9140193a07e41a78fd0d0dd0db5b60f7f50622d5
Muhammed Haque <nohorjo@gmail.com>
2018-03-16 21:22:57 +0000

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,60 +1,9 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Alert
-} from 'react-native';
-import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
+import { StackNavigator } from 'react-navigation';
+
 import FBLoginView from './FBLoginView'
-import { authenticate } from './Remote';
 
-const loginError = () => Alert.alert("Error with Facebook login");
-
-async function login(e) {
-  if (!(e.type == 'success' && await authenticate(e))) {
-    loginError();
-  }
-}
-
-const nothing = () => null;
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Centsa
-        </Text>
-        <FBLogin
-          buttonView={<FBLoginView />}
-          ref={(fbLogin) => { this.fbLogin = fbLogin }}
-          loginBehavior={FBLoginManager.LoginBehaviors.Native}
-          permissions={["email"]}
-          onLogin={login}
-          onLoginFound={nothing}
-          onLoginNotFound={nothing}
-          onLogout={nothing}
-          onCancel={loginError}
-          onPermissionsMissing={loginError}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 20,
-  },
+const App = StackNavigator({
+  Home: { screen:FBLoginView },
 });
+
+export default App;
--- src/FBLoginView.js
+++ src/FBLoginView.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Alert
+  } from 'react-native';
 import PropTypes from 'prop-types';
+import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
+
 var Icon = require('react-native-vector-icons/FontAwesome');
 
+import { authenticate } from './Remote';
+
+const loginError = () => Alert.alert("Error with Facebook login");
+const nothing = () => null;
+
+async function login(e) {
+  if (e.type == 'success' && await authenticate(e)) {
+  } else {
+    loginError();
+  }
+}
+
 export default class FBLoginView extends Component {
+    static navigationOptions = {
+        header: null
+    };
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Centsa
+          </Text>
+                <FBLogin
+                    buttonView={<FBLoginButton />}
+                    ref={(fbLogin) => { this.fbLogin = fbLogin }}
+                    loginBehavior={FBLoginManager.LoginBehaviors.Native}
+                    permissions={["email"]}
+                    onLogin={login}
+                    onLoginFound={nothing}
+                    onLoginNotFound={nothing}
+                    onLogout={nothing}
+                    onCancel={loginError}
+                    onPermissionsMissing={loginError}
+                />
+            </View>
+        );
+    }
+}
+
+class FBLoginButton extends Component {
     static contextTypes = {
         isLoggedIn: PropTypes.bool,
         login: PropTypes.func,
@@ -38,3 +84,18 @@ export default class FBLoginView extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 30,
+      textAlign: 'center',
+      margin: 20,
+    },
+  });
+  
